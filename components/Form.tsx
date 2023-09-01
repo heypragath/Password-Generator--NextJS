cb3fe3e5f468f12447c10e8307ed425247b7f55a
pragath rao <heypragath@gmail.com>
2023-09-01 17:39:13 +0530

    First Commit

---
+++ components/Form.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Checkbox } from "@/components/ui/checkbox"
+
+
+function Form() {
+
+    
+  return (
+    <div>
+        <Checkbox />
+
+    </div>
+  )
+}
+
+export default Form
